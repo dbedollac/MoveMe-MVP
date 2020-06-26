0f81bd6864f17de5ef19ef5de20385b60ba19955
dbedollac <daniel.bedollac@gmail.com>
2020-06-26 11:15:38 -0500

    Creación de framework

---
+++ src/Components/firestore.js
@@ -0,0 +1,18 @@
+import firebase from ‘firebase’;
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyCICLsJYEeDK_Ji5VIQGyFp4UIcTJYhyw0",
+  authDomain: "moveme-ad742.firebaseapp.com",
+  databaseURL: "https://moveme-ad742.firebaseio.com",
+  projectId: "moveme-ad742",
+  storageBucket: "moveme-ad742.appspot.com",
+  messagingSenderId: "50656092485",
+  appId: "1:50656092485:web:aaffe261341d07cec65e2c",
+  measurementId: "G-903QLQCN8H"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+export default firebase;
